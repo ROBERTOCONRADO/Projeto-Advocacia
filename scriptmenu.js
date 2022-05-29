624fc71eca5b700de1617c37b848b6c97744235d
RobertoConrado <robertoconradorc@gmail.com>
2022-05-29 15:29:38 -0300

    ADV

---
+++ scriptmenu.js
@@ -0,0 +1,24 @@
+const menuItens = document.querySelectorAll('.nav-links li a[href^="#"]');
+
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function getScrollTopHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const to = getScrollTopHref(event.target);
+
+    scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+    window.scroll({
+        top:to,
+        behavior: 'smooth'
+    });
+}
